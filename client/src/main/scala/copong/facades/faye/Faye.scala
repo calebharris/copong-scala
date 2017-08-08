7daa4062bf25c9e6142c3533b6e80dc505303aec
Caleb Harris <caleb.harris@mac.com>
2017-08-08 12:11:41 -0400

    Add publish to Faye.Client facade

--- client/src/main/scala/copong/facades/faye/Faye.scala
+++ client/src/main/scala/copong/facades/faye/Faye.scala
@@ -17,6 +17,7 @@ object Faye extends js.Object {
   @js.native
   class Client(val endpoint: String) extends js.Object {
     def subscribe[A <: js.Object](channel: String, callback: js.Function1[A, Unit]): Subscription = js.native
+    def publish[A <: js.Object](channel: String, event: A): Unit = js.native
   }
 
 }
